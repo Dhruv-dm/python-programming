2bbfeb871fe5fcffad6e88244269a8e1b0bd61b2
Dhruv-dm <dhruvmukheja98@gmail.com>
2026-05-23 14:06:11 +0530

    update Hit and Trial

--- Practice Questions/Hit and Trial.py
+++ Practice Questions/Hit and Trial.py
@@ -1,3 +1,9 @@
-stng = 'nice to Have you'
-rest = stng.replace(" ","").lower()
-print(rest)
+arr = [1,2,3,4,5]
+lst = []
+def alternate_num(arry):
+    for i in range(len(arry)):
+        if i%2 == 0:
+            lst.append(arry[i])
+    print(lst)
+
+alternate_num(arr)
